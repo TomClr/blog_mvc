d297b888063a943029cbbe8b7150c80e55ab3496
Thomas Claireau <thomas.claireau@gmail.com>
2019-01-30 11:43:18 +0100

    ajout d'un routeur index.php et création du fichier controller.php

--- index.php
+++ index.php
@@ -1,6 +1,25 @@
 <?php
-require('model.php');
+/* require('model.php');
 
 $posts = getPosts();
 
-require('indexView.php');
+require('indexView.php'); */
+
+require('controller.php');
+
+if (isset($_GET['action'])) {
+    if ($_GET['action'] == 'listPosts') {
+        listPosts();
+    }
+    elseif ($_GET['action'] == 'post') {
+        if (isset($_GET['id']) && $_GET['id'] > 0) {
+            post();
+        }
+        else {
+            echo 'Erreur : aucun identifiant de billet envoyé';
+        }
+    }
+}
+else {
+    listPosts();
+}
