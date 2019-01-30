74692535d349671ed3087401807bf79ea7742e38
Thomas Claireau <thomas.claireau@gmail.com>
2019-01-30 14:49:05 +0100

    amélioration de la gestion des erreurs avec try catch

--- model/frontend.php
+++ model/frontend.php
@@ -29,15 +29,8 @@ function getComments($postId)
 // Nouvelle fonction qui nous permet d'éviter de répéter du code
 function dbConnect()
 {
-    try
-    {
         $db = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
         return $db;
-    }
-    catch(Exception $e)
-    {
-        die('Erreur : '.$e->getMessage());
-    }
 }
 
 function postComment($postId, $author, $comment) {
